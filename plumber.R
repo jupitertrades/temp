469870f2f502625a062afab104ce9b61bf01afd8
simitpatel <simitp@gmail.com>
2018-02-23 12:18:55 -0500

    Add files via upload

---
+++ plumber.R
@@ -0,0 +1,23 @@
+#load libraries
+
+library(RPostgreSQL)
+library(plumber)
+
+#establish database connections
+
+drv <- dbDriver("PostgreSQL")
+pgconn <- dbConnect(drv, dbname="pg1",
+                    host="pdb1.c7asxuecrarw.us-west-2.rds.amazonaws.com", port=5432,
+                    user="postgreskadmin", password="ht534!nk")
+
+# start plumbering
+
+#' Return the description of the ticker received
+#' @param tick The ticker whose description should be returned to the user
+#' @get /description
+function(tick=""){
+  
+  descript <- dbGetQuery(pgconn,'SELECT ticker,long_description FROM companydescription')
+  descript_show <- descript %>% filter(ticker==tick)
+  return(descript_show$long_description)
+}
